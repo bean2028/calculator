a8a83547cdb524cb4c243900b7a6810fc07e2830
Eric Helstad <erichelstad@gmail.com>
2023-01-02 14:46:54 -0600

    Add snarky message regarding dividing by zero

--- js/calculator.js
+++ js/calculator.js
@@ -92,7 +92,8 @@ function operate(operator, num1, num2) {
         return multiply(num1, num2);
     }
     else if (operator == '/') {
-        return divide(num1, num2);
+        if (num2 == 0) {return "Can't divide by 0 :)"}
+        else {return divide(num1, num2)}
     }
     else {
         return "Not a valid operator";
